469a8fc6e25c59e7a0463f73869114abe95fac6c
Vincent Enriquez <enriquezroseller23@gmail.com>
2023-06-06 00:03:42 +0800

    replacing error modal visible to open, replacing usestate in user inputs to useref.

--- src/Components/Users/AddUsers.js
+++ src/Components/Users/AddUsers.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Button from "../UI/Button/Button";
 import Card from "../UI/Card/Card";
 import ErrorModal from "../UI/Modal/ErrorModal";
@@ -6,50 +6,46 @@ import ErrorModal from "../UI/Modal/ErrorModal";
 import "./AddUsers.css";
 
 const AddUsers = (props) => {
-  const [userValue, setUserValue] = useState("");
-  const [userAge, setUserAge] = useState("");
   const [error, setError] = useState(null);
 
-  const userValueHandler = (e) => {
-    setUserValue(e.target.value);
-  };
-
-  const userAgeHandler = (e) => {
-    setUserAge(e.target.value);
-  };
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
 
   const submitHandler = (e) => {
     e.preventDefault();
 
-    if (userValue.trim().length === 0 || userAge.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const eneteredAge = ageInputRef.current.value;
+
+    if (enteredName.trim().length === 0 || eneteredAge.trim().length === 0) {
       setError(`Invalid Input! You have to provide input`);
       return;
     }
 
-    if (userValue.length < 4 || userValue.length > 50) {
+    if (enteredName.length < 4 || enteredName.length > 50) {
       setError(`Invalid Age! Name length should atleast be (6-50)`);
       return;
     }
 
-    if (isNaN(userAge) || +userAge < 1 || +userAge > 99) {
+    if (isNaN(eneteredAge) || +eneteredAge < 1 || +eneteredAge > 99) {
       setError("Please enter a valid Age (1-99)");
       return;
     }
 
     const symbolRegex = /^[A-Za-z\s]+$/;
-    if (!symbolRegex.test(userValue)) {
+    if (!symbolRegex.test(enteredName)) {
       setError(`Invalid Input! Name should not contain numbers.`);
       return;
     }
 
-    const upperCasedValue = userValue
+    const upperCasedValue = enteredName
       .split(" ")
       .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
       .join(" ");
 
-    props.onAddUser(upperCasedValue, userAge);
-    setUserValue("");
-    setUserAge("");
+    props.onAddUser(upperCasedValue, eneteredAge);
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
   const closeModal = () => {
@@ -63,21 +59,9 @@ const AddUsers = (props) => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="name">Name (6-50 Characters)</label>
-            <input
-              id="name"
-              type="text"
-              min={6}
-              max={50}
-              value={userValue}
-              onChange={userValueHandler}
-            />
+            <input id="name" type="text" min={6} max={50} ref={nameInputRef} />
             <label htmlFor="age">Age (Years)</label>
-            <input
-              id="age"
-              type="number"
-              value={userAge}
-              onChange={userAgeHandler}
-            />
+            <input id="age" type="number" ref={ageInputRef} />
             <Button onClick={submitHandler}>Add User</Button>
           </div>
         </form>
